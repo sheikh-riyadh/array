79f0ae9af3f0879cbdcd9a7fd51d90f59475d811
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 14:37:43 +0600

    array

---
+++ array.js
@@ -0,0 +1,15 @@
+let fruits = ["Apple", "Banana", "Orange"];
+
+/* Find index number of banana */
+let indexOfBanana = fruits.indexOf("Banana");
+console.log(indexOfBanana);
+
+/* Replace banana with mango */
+fruits[1] = "Mango";
+
+/* Remove orange */
+fruits.pop()
+
+/* Add watermelon in last element */
+fruits.push("Watermelon");
+console.log(fruits);
